56e5ae28f0536cfddd5d40619302253b7c1c3776
Saulo de Tarso <de_tarso7@hotmail.com>
2018-10-26 00:51:20 -0300

    Criando as primeiras navegações.

--- src/pages/tab-categoria/tab-categoria.ts
+++ src/pages/tab-categoria/tab-categoria.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the CategoriaPage page.
+ * Generated class for the TabCategoriaPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,20 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-categoria',
-  templateUrl: 'categoria.html',
+  selector: 'page-tab-categoria',
+  templateUrl: 'tab-categoria.html',
 })
-export class CategoriaPage {
+export class TabCategoriaPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriaPage');
-  }
-
-  abrirProdutos(): void {
-    this.navCtrl.setRoot('TabsPage')
+    console.log('ionViewDidLoad TabCategoriaPage');
   }
 
 }
